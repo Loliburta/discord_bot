ed818c51a38428173d5f98142b2a7253531e198d
Loliburta <mateusz.maranowski@protonmail.com>
2021-04-25 19:34:13 +0200

    added sending messages

--- src/bot.js
+++ src/bot.js
@@ -3,5 +3,36 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 
 const client = new Client();
+const prefix = "!";
 
-client.login(process.env.DISCORDJS_BOT_TOKEN)
+client.on("ready", () => {
+  console.log(`${client.user.username} has logged in`);
+});
+client.on("message", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  console.log(`[${message.author.tag}] ${message}`);
+  if (message.content === "hello") {
+    message.channel.send("hey");
+  }
+  if (message.content.startsWith(prefix)) {
+    const [commandName, ...args] = message.content
+      .trim()
+      .substring(prefix.length)
+      .split(/\s+/);
+    console.log(commandName, args);
+    if (commandName === "gamble" && !isNaN(parseInt(args[0])) && parseInt(args[0]) > 0) {
+      console.log(parseInt(args[0]))
+      Math.random() >= 0.5
+        ? message.channel.send(
+            `${message.author} Wygrał ${parseInt(args[0]) * 2} żołędzi`
+          )
+        : message.channel.send(
+            `${message.author} Przejebał ${parseInt(args[0])} żołędzi`
+          );
+    }
+  }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
